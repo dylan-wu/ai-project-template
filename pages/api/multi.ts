4ab0d798f1526542ce21ff7a8d1a47982876be94
Dylan-Wu <dylanwu@live.com>
2023-02-05 10:30:30 -0600

    Initial commit

---
+++ pages/api/multi.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const input1 = req.query.input1
+  const input2 = req.query.input2
+
+  const response = await fetch("", {
+    method: "POST",
+    body: `{"args": ["${input1}", "${input2}"], "kwargs": {}, "apiKey": ""}`,
+    // body: `{"args": ["Transform the following passage in the style of", "When a spacecraft enters the atmosphere of a planet, it’s hit with aerodynamic forces, which help slow it down. On Mars, where the atmosphere is less than 1% the density of Earth’s atmosphere, extra help is needed to create the drag necessary to slow and safely land a spacecraft.","Pirates"], "kwargs": {}, "apiKey": "e9953b1e-68ac-4716-b403-a2c10b8a9d10"}`,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  res.status(200).json(await response.json())
+}
---
+++ pages/index.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link'
+import { useState } from 'react'
+
+export default function Home() {
+  const [input1, setInput1] = useState("")
+  const [input2, setInput2] = useState("")
+  const [data, setData] = useState({outputs: [""]})
+  async function onSubmit() {
+    const response = await fetch(`api/multi?input1=${input1}&input2=${input2}`, {
+      method: "GET",
+    })
+    setData(await response.json())
+    return data
+  }
+  return (
+    <>
+    <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+      <div className="text-center mb-16">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Lorem Ipsum
+        </h1>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Lorem ipsum dolor sat amet.
+        </p>
+      </div>
+      <div className="flex flex-col gap-4 mb-4">
+        <input className="border-2 border-grey-300 px-4 py-2 rounded-md" value={input1} onChange={e => setInput1(e.target.value)} placeholder="Input 1"/>
+        <input className="border-2 border-grey-300 px-4 py-2 rounded-md" value={input2} onChange={e => setInput2(e.target.value)} placeholder="Input 2"/>
+        <button className="border-2 border-rose-200 px-4 py-2 rounded-md text-black font-semibold bg-rose-300" onClick={onSubmit}>Generate</button>
+      </div>
+      <div className="border-2 border-rose-200 px-4 py-2 rounded-md h-full">
+        <p className={`${data.outputs[0] ? "text-black" : "text-rose-400 italic"}`}>
+          {data.outputs[0] ? data.outputs[0] : "Output will appear here..."}
+        </p>
+      </div>
+    </div>
+  </>
+  )
+}
